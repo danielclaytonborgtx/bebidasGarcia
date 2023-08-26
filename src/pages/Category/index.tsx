7c85f9434c99409d623371c9490d246b45943e90
danielclayton <danielclaitul@gmail.com>
2023-08-26 20:44:57 -0300

    create route Category e Products

---
+++ src/pages/Category/index.tsx
@@ -0,0 +1,3 @@
+export function Category() {
+  return <span>CATEGORY</span>;
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
-
+import { Category } from "../pages/Category";
+import { Products } from "../pages/Products";
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Category" element={<Category />} />
+      <Route path="/Products" element={<Products />} />
     </Routes>
   );
 }
