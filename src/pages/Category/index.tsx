3bdb4449041ef0b11d055ba5083f47d07ccc9f1b
danielclayton <danielclaitul@gmail.com>
2023-08-28 21:25:50 -0300

    css initial

--- src/pages/Category/index.tsx
+++ src/pages/Category/index.tsx
@@ -3,13 +3,9 @@ export function Category() {
     <div>
       <h1>BEBIDAS GARCIA</h1>
       <a href="./Bebidas">Bebidas</a>
-      <br />
       <a href="./Salgados">Salgados</a>
-      <br />
       <a href="./Outros">Outros</a>
-      <br />
       <a href="./">Sair</a>
-      <br />
     </div>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,10 @@
+import "./styles.css";
+
 export function Home() {
   return (
     <div>
       <h1>BEBIDAS GARCIA</h1>
-      <a href="./Category">Veja nossos produtos</a>
+      <a href="./Category">Veja nossos produtos e nossos preços</a>
     </div>
   );
 }
