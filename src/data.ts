c34a3d7ff2cb31c25c7b7ee4e5bd01f53a16ee94
Paulo André <pauloandre0011@gmail.com>
2023-09-01 20:32:33 -0300

    Add data

---
+++ src/data.ts
@@ -0,0 +1,123 @@
+export const data = [
+  {
+    id: 1,
+    name: "Bebidas",
+    image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+    subCategory: [
+      {
+        id: 1,
+        name: "Cervejas",
+        image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+        products: [
+          {
+            id: 1,
+            name: "Skol",
+            image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+          },
+          {
+            id: 2,
+            name: "Brahma",
+            image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+          },
+          {
+            id: 3,
+            name: "Antartica",
+            image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+          },
+          {
+            id: 4,
+            name: "Budweiser",
+            image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: "Refrigerantes",
+        image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+        products: [
+          {
+            id: 1,
+            name: "Coca-Cola",
+            image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+          },
+          {
+            id: 2,
+            name: "Fanta",
+            image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+          },
+          {
+            id: 3,
+            name: "Guaraná",
+            image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+          },
+          {
+            id: 4,
+            name: "Sprite",
+            image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+          },
+        ],
+      },
+      {
+        id: 5,
+        name: "Sucos",
+        image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+        products: [
+          {
+            id: 1,
+            name: "Laranja",
+            image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+          },
+          {
+            id: 2,
+            name: "Uva",
+            image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+          },
+          {
+            id: 3,
+            name: "Limão",
+            image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+          },
+          {
+            id: 4,
+            name: "Maracujá",
+            image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Comida",
+    image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+    subCategory: [
+      {
+        id: 1,
+        name: "Lanches",
+        image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+        products: [
+          {
+            id: 1,
+            name: "X-Burguer",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Caldos",
+        image: "https://i.ibb.co/0j3qQYR/bebidas.jpg",
+        products: [
+          {
+            id: 1,
+            name: "Caldo de Feijão",
+          },
+          {
+            id: 2,
+            name: "Caldo de Mandioca",
+          },
+        ],
+      },
+    ],
+  },
+];
--- src/pages/Category/index.tsx
+++ src/pages/Category/index.tsx
@@ -1,6 +1,28 @@
 import { Link } from "react-router-dom";
 
 export function Category() {
+  const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  array.forEach((item) => {
+    console.log(item);
+  });
+
+  const map = array.map((item) => {
+    return item * 10;
+  });
+
+  const filter = array.filter((item) => {
+    return item > 5;
+  });
+
+  const find = array.find((item) => {
+    return item === 5;
+  });
+
+  console.log("MAP", map);
+  console.log("Filter", filter);
+  console.log("find", find);
+
   return (
     <div>
       <h1>Categorias</h1>
